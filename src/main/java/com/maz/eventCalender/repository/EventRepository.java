a5716a1b55c01cca1d7b86f9565f0e481e63ef17
Mazhar <mazghul@gmail.com>
2019-02-25 00:38:11 +0530

    Adding queries for email and period

--- src/main/java/com/maz/eventCalender/repository/EventRepository.java
+++ src/main/java/com/maz/eventCalender/repository/EventRepository.java
@@ -3,17 +3,21 @@ package com.maz.eventCalender.repository;
 import com.maz.eventCalender.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
-   @Query("SELECT event FROM event as event where event.to_email_id = :toEmailId")
-   public List<Event> findByEmail(String toEmailId);
+   @Query(value = "SELECT id, subject, end_date, start_date, attachment, subject, to_email_id FROM `event` where to_email_id = :toEmailId", nativeQuery = true)
+   List<Event> findByToEmail(@Param("toEmailId") String toEmailId);
 
 
-    //@Query("Select event from Event as event where event_id between :startDate and  :endDate")
-   // public List<Event> getEventsByPeriod(String startDate, String endDate);
+    @Query(value = "SELECT id, subject, end_date, start_date, attachment, subject, to_email_id  FROM event where" +
+            " (start_date > :startDate and end_date < :endDate) " +
+            "OR (:startDate between start_date and end_date)" +
+            " OR (:endDate between start_date and end_date)", nativeQuery = true)
+    List<Event> getEventsByPeriod(@Param("startDate") String startDate, @Param("endDate") String endDate);
 
     //SELECT * FROM `event`where (start_date BETWEEN "2019-01-01T09:00" and  "2019-01-01T11:00") or (end_date BETWEEN "2019-01-01T09:00" and  "2018-01-01T11:00")
 }
